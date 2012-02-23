8f7f0c11f6fcbbe201f7318850efd803dce3385a
Lars-Erik Aabech <lea@markedspartner.no>
2012-02-23 15:04:14 +0100

    Added new script files from prev. commit

---
+++ Scripts/Mock.js
@@ -0,0 +1,28 @@
+﻿	var Mock = Class.extend({
+		logged: [],
+		calls: [],
+		logCalls: 0,
+		reset: function () {
+			this.logCalls = 0;
+			this.logged = [];
+			this.calls = [];
+		},
+		log: function (name, args) {
+			this.logCalls++;
+			if (this.shouldLog(name))
+				this.calls.push({ name: name, args: this.getCallInfo(args) });
+		},
+		shouldLog: function (name) {
+			return this.logged.indexOf(name) > -1;
+		},
+		getCallInfo: function (args) {
+			var calleeStr = args.callee.toString();
+			var paramStr = calleeStr.substring(calleeStr.indexOf("(") + 1, calleeStr.indexOf(")"));
+			var params = paramStr.split(",");
+			var obj = {};
+			for (var i = 0; i < params.length; i++) {
+				obj[params[i].trim()] = args[i];
+			}
+			return obj;
+		}
+	});
---
+++ Scripts/MockContext.js
@@ -0,0 +1,13 @@
+﻿	var MockContext = Mock.extend({
+		save: function () { this.log("save", arguments); },
+		restore: function () { this.log("restore", arguments); },
+		strokeRect: function (x,y,w,h) { this.log("strokeRect", arguments); },
+		beginPath: function () { this.log("beginPath", arguments); },
+		closePath: function () { this.log("closePath", arguments); },
+		stroke: function () { this.log("stroke", arguments); },
+		moveTo: function (x,y) { this.log("moveTo", arguments); },
+		lineTo: function (x,y) { this.log("lineTo", arguments); },
+		translate: function (x,y) { this.log("translate", arguments); },
+		rotate: function (angle) { this.log("rotate", arguments); },
+		fillText: function (text,x,y,maxWidth) { this.log("fillText", arguments); }
+	});
