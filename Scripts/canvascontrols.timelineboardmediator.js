e83f381b920c7707a82870e706622c4559e4769d
Sami Ylonen <sami.ylonen@gmail.com>
2012-04-10 14:48:41 +0200

    timelineboardmediator

---
+++ Scripts/canvascontrols.timelineboardmediator.js
@@ -0,0 +1,38 @@
+﻿(function (cc) {
+
+	cc.TimelineBoardMediator = Class.extend({
+		init: function (timeline, board, timelineCanvas, boardCanvas) {
+			this.timeline = timeline;
+			this.board = board;
+			this.timelineCanvas = timelineCanvas;
+			this.boardCanvas = boardCanvas;
+
+			timeline.on("demandRedraw.cc nodeClicked.cc", this, this._onTimelineRedrawDemanded);
+			timeline.on("periodChanged.cc", this, this._onPeriodChange);
+			timeline.on("drag.cc", this, this._onTimelineDragged);
+			board.on("mousemove", this, this._onMouseMove);
+			board.on("demandRedraw.cc", this, this._onBoardRedrawDemanded);
+		},
+		_onTimelineRedrawDemanded: function (sender, data) {
+			this.timelineCanvas.paint();
+			this.boardCanvas.paint();
+		},
+		_onBoardRedrawDemanded: function (sender, data) {
+			this.boardCanvas.paint();
+		},
+		_onPeriodChange: function (sender, data) {
+			this.timelineCanvas.paint();
+			this.board.setPeriod(data.period);
+			this.boardCanvas.paint();
+		},
+		_onMouseMove: function (sender, data) {
+			this.timeline._raise("mousemove", data);
+		},
+		_onTimelineDragged: function (sender, data) {
+			this.board.setOffset(data.offset);
+			this.boardCanvas.paint();
+		}
+
+	});
+
+})(canvascontrols);
